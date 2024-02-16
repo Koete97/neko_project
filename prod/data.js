8a0df558b16b86dc3b0a02de611755ed0b6b7ad4
Koete97 <157823390+Koete97@users.noreply.github.com>
2024-02-16 19:05:41 +0200

    Create data.js

---
+++ prod/data.js
@@ -0,0 +1,36 @@
+'use strict';
+
+
+
+export const blogPosts = [
+  {
+    image: '../images/one (2).jpg',
+    heading: 'Business workshop',
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus nisi, fermentum non elit vitae, laoreet malesuada nibh. Integer nunc sem, eleifend et quam sed, lobortis maximus velit. Mauris aliquam nisi id erat feugiat eleifend maximus quis justo. Aenean tempus lorem vel lacus bibendum.",
+  },
+  {
+    image: '../images/two.jpg',
+    heading: 'Skykingdom TV',
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus nisi, fermentum non elit vitae, laoreet malesuada nibh. Integer nunc sem, eleifend et quam sed, lobortis maximus velit. Mauris aliquam nisi id erat feugiat eleifend maximus quis justo. Aenean tempus lorem vel lacus bibendum.",
+  },
+  {
+    image: '../images/three.jpg',
+    heading: 'Coming Events',
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus nisi, fermentum non elit vitae, laoreet malesuada nibh. Integer nunc sem, eleifend et quam sed, lobortis maximus velit. Mauris aliquam nisi id erat feugiat eleifend maximus quis justo. Aenean tempus lorem vel lacus bibendum.",
+  },
+  {
+    image: '../images/four.jpg',
+    heading: 'Comedy Events',
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus nisi, fermentum non elit vitae, laoreet malesuada nibh. Integer nunc sem, eleifend et quam sed, lobortis maximus velit. Mauris aliquam nisi id erat feugiat eleifend maximus quis justo. Aenean tempus lorem vel lacus bibendum.",
+  },
+  {
+    image: '../images/five.jpg',
+    heading: 'A toast from the top: Bofihla leads a celebration',
+    body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus nisi, fermentum non elit vitae, laoreet malesuada nibh. Integer nunc sem, eleifend et quam sed, lobortis maximus velit. Mauris aliquam nisi id erat feugiat eleifend maximus quis justo. Aenean tempus lorem vel lacus bibendum.",
+  },
+  {
+    image: '../images/six (2).jpg',
+    heading: 'Charity campaign',
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lacus nisi, fermentum non elit vitae, laoreet malesuada nibh. Integer nunc sem, eleifend et quam sed, lobortis maximus velit. Mauris aliquam nisi id erat feugiat eleifend maximus quis justo. Aenean tempus lorem vel lacus bibendum.",
+  },
+];
